63cee222c5163c669ea4c2fdf81c30bd3738e709
Fatima-Zaid <fatimaameer957@gmail.com>
2025-10-21 09:22:32 +0300

    Set up App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import './App.css'
+import { useEffect, useState } from "react"
+import axios from "axios"
+import "./App.css"
 
 const App = () => {
+  const [posts, setPosts] = useState([])
 
-  return (
-    <>
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        let response = await axios.get()
+        setPosts(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getPosts()
+  }, [])
 
-    </>
-  )
+  return (<>
+
+
+  </>)
 }
 
 export default App
---
+++ src/components/Post.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+import axios from "axios"
+
+const Post = ()=>{
+
+  
+
+}
+
+export default Post
